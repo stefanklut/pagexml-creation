47b3ef363b4b7be77f6f10ae042cc68fa9686d4e
Stefan Klut <stefanklut12@gmail.com>
2023-11-02 13:30:08 +0100

    Working generation of image regions

--- pagexml_from_overlay_images.py
+++ pagexml_from_overlay_images.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import random
+import uuid
 from multiprocessing import Pool
 from pathlib import Path
 from typing import Optional, Sequence
@@ -10,8 +11,29 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 
+from page_xml.xmlPAGE import PageData
 from utils.input_utils import get_file_paths, supported_image_formats
 from utils.logging_utils import get_logger_name
+from utils.path_utils import image_path_to_xml_path
+
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Main file for Layout Analysis")
+
+    io_args = parser.add_argument_group("IO")
+    io_args.add_argument(
+        "-f", "--foreground", help="Foreground folder/file", nargs="+", action="extend", type=str, required=True
+    )
+    io_args.add_argument(
+        "-b", "--background", help="Foreground folder/file", nargs="+", action="extend", type=str, required=True
+    )
+    io_args.add_argument("-o", "--output", help="Output folder", type=str)
+
+    parser.add_argument("-n", "--number", type=int, help="Number of images generated")
+
+    args = parser.parse_args()
+
+    return args
 
 
 class Creator:
@@ -94,13 +116,13 @@ class Creator:
 
     @staticmethod
     def within_rectangle(points, rectangle):
-        if np.any(rectangle[0, 0] > corners[:, 0]):
+        if np.any(rectangle[0, 0] > points[:, 0]):
             return False
-        if np.any(corners[:, 0] > rectangle[1, 0]):
+        if np.any(points[:, 0] > rectangle[1, 0]):
             return False
-        if np.any(rectangle[0, 1] > corners[:, 1]):
+        if np.any(rectangle[0, 1] > points[:, 1]):
             return False
-        if np.any(corners[:, 1] > rectangle[1, 1]):
+        if np.any(points[:, 1] > rectangle[1, 1]):
             return False
 
         return True
@@ -116,48 +138,47 @@ class Creator:
 
         max_trans_x = background_width - scaled_foreground_width
         max_trans_y = background_height - scaled_foreground_height
-        trans_x = random.randint(0, max_trans_x)
-        trans_y = random.randint(0, max_trans_y)
 
-        angle = random.uniform(-30, 30)
-        M = cv2.getRotationMatrix2D((scaled_foreground_width / 2, scaled_foreground_height / 2), angle, 1)
+        max_iters = 1000
 
-        M[:, 2] += [trans_x, trans_y]
+        for i in range(max_iters):
+            trans_x = random.randint(0, max_trans_x)
+            trans_y = random.randint(0, max_trans_y)
+            angle = random.uniform(-30, 30)
+            M = cv2.getRotationMatrix2D((scaled_foreground_width / 2, scaled_foreground_height / 2), angle, 1)
+
+            M[:, 2] += [trans_x, trans_y]
+
+            corners = cv2.transform(
+                np.array(
+                    [
+                        [
+                            [0, 0],
+                            [scaled_foreground_width, 0],
+                            [scaled_foreground_width, scaled_foreground_height],
+                            [0, scaled_foreground_height],
+                        ]
+                    ],
+                    dtype=np.float32,
+                ),
+                M,
+            )
+            corners = corners.squeeze().astype(int)
+
+            if self.within_rectangle(corners, np.array([(0, 0), (background_width, background_height)])):
+                break
+        else:
+            raise ValueError(f"No valid image location found within {max_iters} tries")
+
         image_affine = cv2.warpAffine(scaled_foreground_image, M, (background_width, background_height))
         mask = cv2.warpAffine(np.ones_like(scaled_foreground_image) * 255, M, (background_width, background_height))
 
         image_result = background_image.copy()
         image_result[np.nonzero(mask)] = image_affine[np.nonzero(mask)]
 
-        corners = cv2.transform(
-            np.array(
-                [
-                    [
-                        [0, 0],
-                        [0, scaled_foreground_height],
-                        [scaled_foreground_width, 0],
-                        [scaled_foreground_width, scaled_foreground_height],
-                    ]
-                ],
-                dtype=np.float32,
-            ),
-            M,
-        )
-        corners = corners.squeeze().astype(int)
-
-        # for corner in corners:
-        #     image = cv2.circle(image_result, corner, 5, (255, 0, 0), -1)
-
-        if np.any(np.logical_or(0 > corners[:, 0], corners[:, 0] > background_width)) or np.any(
-            np.logical_or(0 > corners[:, 1], corners[:, 1] > background_height)
-        ):
-            self.logger.warning(f"Corner out of bounds. Corners: {corners} Bounds: {background_width, background_height}")
-
         output_path = "test.jpg"
 
-        cv2.imwrite(output_path, image_result)
-
-        return corners
+        return image_result, corners
 
     def random_foreground_path(self):
         if self.foreground_paths is None:
@@ -177,9 +198,44 @@ class Creator:
             random.shuffle(self._random_background_paths)
         return self._random_background_paths.pop()
 
-    def create_page(self, i):
-        foreground_image = cv2.imread(foreground_path)
-        background_image = cv2.imread(background_path)
+    def create_page(self, items):
+        if self.output_dir is None:
+            raise TypeError("Cannot run when the output_dir is None")
+        foreground_path, background_path, i = items
+        foreground_image = cv2.imread(str(foreground_path))
+
+        background_image = cv2.imread(str(background_path))
+
+        image, corners = self.overlay_images_random_transform(foreground_image, background_image)
+
+        image_path = self.output_dir.joinpath(f"{i}.jpg")
+
+        cv2.imwrite(str(image_path), image)
+
+        page_dir = self.output_dir.joinpath("page")
+
+        if not page_dir.is_dir():
+            self.logger.info(f"Could not find page dir ({page_dir}), creating one at specified location")
+            page_dir.mkdir(parents=True)
+
+        xml_path = image_path_to_xml_path(image_path, check=False)
+
+        page = PageData(xml_path)
+        page.new_page(image_path.name, str(image.shape[0]), str(image.shape[1]))
+
+        region_coords = ""
+        for coords in corners.reshape(-1, 2):
+            region_coords = region_coords + f" {coords[0]},{coords[1]}"
+        region_coords = region_coords.strip()
+
+        region_type = "ImageRegion"
+        region = "Photo"
+        region_id = 1
+
+        _uuid = uuid.uuid4()
+        image_reg = page.add_element("ImageRegion", f"region_{_uuid}_{region_id}", region, region_coords)
+
+        page.save_xml()
 
     def process(self):
         if self.foreground_paths is None:
@@ -194,13 +250,15 @@ class Creator:
         if self.number is None:
             raise TypeError("Cannot run when the number is None")
 
+        items = [(self.random_foreground_path(), self.random_background_path(), i) for i in range(self.number)]
+
         # Single threaded
         # for image_path in tqdm(image_paths):
         #     generate_empty_page_xml(image_path)
 
         # Multi threading
         with Pool(os.cpu_count()) as pool:
-            _ = list(tqdm(pool.imap_unordered(self.create_page, range(self.number)), total=len(self.number)))
+            _ = list(tqdm(pool.imap_unordered(self.create_page, items), total=self.number))
 
 
 def main(args: argparse.Namespace):
@@ -209,14 +267,10 @@ def main(args: argparse.Namespace):
         background_paths=args.background,
         output_dir=args.output,
         number=args.number,
-        copy=args.copy,
     )
     creator.process()
 
 
 if __name__ == "__main__":
-    creator = Creator()
-    creator.overlay_images_random_transform(
-        "/home/stefan/Documents/SURFdrive/Shared/ovdr_images/photo/0b74d52a-5601-6794-9b98-0101b4de8777.jpg",
-        "/home/stefan/Documents/SURFdrive/Shared/ovdr_images/document/0a04f473-3586-faa3-ca9a-874e2d6231ff.jpg",
-    )
+    args = get_arguments()
+    main(args)
